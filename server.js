bb299afd0ff65e3f7af32e5c8266748279022e6b
Aliaksiej Razumaŭ <belarusaed@gmail.com>
2022-01-03 16:07:59 +0300

    Fixed issue#1

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const app = express();
 app.set('view engine', 'pug');
 app.use(express.static('dist'));
 
-app.get('/player', (req, res) => res.render('player', req.query));
+app.get('/player', (req, res) => res.render('player', {
+  ...req.query,
+  fonts: Array.isArray(req.query.fonts) ? req.query.fonts : req.query.fonts ? [req.query.fonts] : []
+}));
 
 app.listen(5000);
